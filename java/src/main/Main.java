bbcdbcfabe1af4e793ed20b66584e2d6efd037f6
gonzalo <gonzalososa342@gmail.com>
2025-01-19 21:49:17 -0300

    Ejecutar ejemplos de patrones de diseño

---
+++ java/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import design_patterns.creational.Singleton;
+import design_patterns.creational.builder.BuilderExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Singleton.main(args);
+		BuilderExample.main(args);
+	}
+
+}
